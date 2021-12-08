c0a4ce3ae746a43629f44d38e9bb22522dcb631e
Danish Danial <danishdanialzurkanain@gmail.com>
2021-12-08 23:11:20 +0800

    Remove not used library

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
-from django.contrib.auth import get_user_model
 from base.models import Room, User
 
 class UserSerializer(ModelSerializer):
